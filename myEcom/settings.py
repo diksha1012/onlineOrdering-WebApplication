357e0750dd9e528f6077c46e4f5868a6314c5108
diksha1012 <76871214+diksha1012@users.noreply.github.com>
2022-07-31 02:41:51 +0530

    changes in allowed host

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ch#erifob$-m25fm&c7tcol8j)+7vzbm$($x=$-&e*^s4c+ta!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000',"online-orderr.herokuapp.com"]
 
 
 # Application definition
