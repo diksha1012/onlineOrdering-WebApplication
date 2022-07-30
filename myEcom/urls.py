93bf2cfe75e3103aff63c834d909111f57309d3b
diksha1012 <76871214+diksha1012@users.noreply.github.com>
2022-07-31 02:54:19 +0530

    changes in allowed host

--- myEcom/urls.py
+++ myEcom/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include, re_path
 from django.conf.urls.static import static
 from . import settings
 from django.views.static import serve
-from django.conf.urls import url
+from django.conf.urls import re_path as url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
