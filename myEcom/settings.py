3e3f5678427b172e4e48c4857bf9964603f65d14
diksha1012 <76871214+diksha1012@users.noreply.github.com>
2022-07-31 19:53:00 +0530

    static import uncommented and changed allowed hosts

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -26,9 +26,8 @@ SECRET_KEY = 'django-insecure-ch#erifob$-m25fm&c7tcol8j)+7vzbm$($x=$-&e*^s4c+ta!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-# ALLOWED_HOSTS = ['127.0.0.1:8000','online-orderr.herokuapp.com']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','online-orderr.herokuapp.com']
 
 
 # Application definition
--- myEcom/urls.py
+++ myEcom/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from . import settings
-# from django.views.static import serve
+from django.views.static import serve
 # from django.conf.urls import  url
 
 urlpatterns = [
